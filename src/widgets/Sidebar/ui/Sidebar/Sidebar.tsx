6db313569da20e002c93369d7c9d40275c10c519
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-06-29 10:28:42 +0300

    add testing component

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -17,13 +17,20 @@ export const Sidebar = ({ className }: SidebarProps) => {
     };
 
     return (
-        <div className={classNames(
-            cls.Sidebar,
-            { [cls.collapsed]: collapsed },
-            [className],
-        )}
+        <div
+            data-testid="sidebar"
+            className={classNames(
+                cls.Sidebar,
+                { [cls.collapsed]: collapsed },
+                [className],
+            )}
         >
-            <Button onClick={onToggle}>Toggle</Button>
+            <Button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+            >
+                Toggle
+            </Button>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher />
